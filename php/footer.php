b2dbac97ab598db9b55847ca0072d1fdb0c523f6
kiran <kiranmrp1310@gmail.com>
2025-06-15 14:01:44 +0530

    index page altered

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark text-white text-center py-4 mt-auto shadow">
+  <div class="container">
+    <div class="mb-3">
+        <a href="https://facebook.com" target="_blank"><i class="bi bi-facebook fs-4"></i></a>
+        <a href="https://instagram.com" target="_blank"><i class="bi bi-instagram fs-4"></i></a>
+        <a href="https://youtube.com" target="_blank"><i class="bi bi-youtube fs-4"></i></a>
+        <a href="https://linkedin.com" target="_blank"><i class="bi bi-linkedin fs-4"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+    </div>
+    <p class="mb-0">&copy; 2025 JJ TECH SOLUTIONS. All rights reserved.</p>
+  </div>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-custom fixed-top shadow">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.html">JJ TECH SOLUTIONS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+
+        <!-- Custom Hover Dropdown for Products -->
+        <li class="nav-item dropdown custom-dropdown">
+          <a class="nav-link" href="products.html" id="productsDropdown">Products</a>
+          <div class="dropdown-menu shadow-lg p-4" aria-labelledby="productsDropdown">
+            <a class="dropdown-item" href="accounts.html">Accounts</a>
+            <a class="dropdown-item" href="#">Intventory</a>
+            <a class="dropdown-item" href="#">Breeder</a>
+            <a class="dropdown-item" href="#">Hatcher</a>
+            <a class="dropdown-item" href="#">Feed Mill</a>
+            <a class="dropdown-item" href="#">Processing Unit</a>
+            <a class="dropdown-item" href="#">Retail Outlet</a>
+            <a class="dropdown-item" href="#">Trading</a>
+          </div>
+        </li>
+
+        <li class="nav-item"><a class="nav-link" href="client.html">Client</a></li>
+        <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
